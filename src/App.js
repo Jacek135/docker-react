fd4d05d2ea87a2ff5d7135d87a91b8a84b6a3b08
Jacek135 <jac.poczatek@gmail.com>
2020-01-06 14:19:28 +0100

    Fix tests

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Witaj Justyno H.-P.
-          Jak sie dzis czujesz??
-          Milego dnia Ci zycze!!
-          Jacek
+        Witaj Justyno H.-P.
+        Jak sie dzis czujesz??
+        Milego dnia Ci zycze!!
+        Jacek
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
